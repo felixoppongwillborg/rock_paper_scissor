7ed2489fcd90f81cbb8e9703c68782bb785c22eb
felixoppongwillborg <felixwillborg@MacBook-Pro.local>
2020-07-08 22:35:34 +0200

    added 3 buttons Rock, Paper & Scissor without any function

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ export class App extends Component {
   render() {
     return (
       <div>
-        
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissor</button>
       </div>
     )
   }
